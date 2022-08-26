f0fc4c25dd7f60e178d2bd7a5d1915d4e270b9f6
Troy <wilson.troy.anthony@gmail.com>
2022-08-25 20:36:21 -0400

    Updated Display method

--- DishesMVC/DishDetailsView.cs
+++ DishesMVC/DishDetailsView.cs
@@ -2,5 +2,22 @@
 {
     public class DishDetailsView
     {
+        public DishDetailsView(Dish dish)
+        {
+            Dish = dish;
+        }
+        public Dish Dish { get; set; }
+
+        public void Display()
+        {
+            string ingredients = string.Join(", ", Dish.Ingredients);
+            //string ingredients = string.Empty;
+            //Dish.Ingredients.ForEach(i => ingredients += i + ", ");
+
+            Console.WriteLine($"Name: {Dish.Name}" +
+                $"\nDescription: {Dish.Description}" +
+                $"\nCalories: {Dish.Calories}" +
+                $"\nIngredients: {ingredients}");
+        }
     }
 }
