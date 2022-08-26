eddf98516825b0a99a8fc1fc06fa2abb89f3c980
Troy <wilson.troy.anthony@gmail.com>
2022-08-25 20:19:21 -0400

    Added dish class

--- DishesMVC/Dish.cs
+++ DishesMVC/Dish.cs
@@ -2,5 +2,17 @@
 {
     public class Dish
     {
+        public Dish(string name, string description, List<string> ingredients, int calories)
+        {
+            Name = name;
+            Description = description;
+            Ingredients = ingredients;
+            Calories = calories;
+        }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<string> Ingredients { get; set; }
+        public int Calories { get; set; }
     }
 }
