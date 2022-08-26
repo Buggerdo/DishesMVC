d9f4d315bfd7a43013907b0521724f079322cc72
Troy <wilson.troy.anthony@gmail.com>
2022-08-25 20:23:59 -0400

    Added DishListView

---
+++ DishesMVC/DishListView.cs
@@ -0,0 +1,21 @@
+﻿namespace DishesMVC
+{
+    public class DishListView
+    {
+        public DishListView(List<Dish> dishes)
+        {
+            Dishes = dishes;
+        }
+
+        public List<Dish> Dishes { get; set; }
+
+        public void Display()
+        {
+            Console.WriteLine("Here is out list of dishes:");
+            foreach(var dish in Dishes)
+            {
+                Console.WriteLine($"{dish.Name} - {dish.Description} - {dish.Calories}");
+            }
+        }
+    }
+}
